77b19d09a761c0be1fc0896be968d8f7519a0d9b
IDoRemember <1015885915@qq.com>
2018-01-30 16:07:23 +0800

    第二阶段

---
+++ public/js/resume.js
@@ -0,0 +1,12 @@
+var $window = $window;
+var loading = $('.loading');
+var intro = $('.intro');
+var containerPage = $('#page');
+
+var images = [];
+
+var Resume = {
+    init:function(){
+        var self = this;
+    }
+}
