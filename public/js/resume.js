7aa84f7159f0c8be49bb9fc5e3d1ff1e352d9d84
IDoRemember <1015885915@qq.com>
2018-02-06 11:56:33 +0800

    提交

--- public/js/resume.js
+++ public/js/resume.js
@@ -1,12 +1,61 @@
-var $window = $window;
+import { bind } from "./C:/Users/Administrator/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/bluebird";
+
+var $window = $(window);
 var loading = $('.loading');
 var intro = $('.intro');
 var containerPage = $('#page');
 
-var images = [];
+var images = [
+    './images/people.png',
+    './images/mountain.png',
+    './images/cloud.png',
+    './images/background.png',
+    './images/pipe.png',
+    './images/trees.png',
+    './images/project.png'
+  ];
+  
 
 var Resume = {
     init:function(){
         var self = this;
+        $window.scrollTop(0);
+        //加载资源
+        resourceHelper.load(images,function(result){
+            loading.fadeOut();
+            self.bindEvent();
+        })
+    },
+    bindEvent:function(){
+        var self = this;
+        var curPosition = 0; // 当前位置
+        var prePosition = 0; // 之前位置
+
+        //监听滚动事件
+        $window.on('scroll',function(e){
+            curPosition = $window.scrollTop();
+            //计算移动距离
+            var distant = curPosition - prePosition;
+            Me.setDirection(distant);
+            Scense.move(curPosition);
+            Me.checkJump(curPosition,prePosition);
+            Me.walk();
+
+            prePosition =  curPosition;
+        });
+
+        $window.on('click','.js-start-resume',function(){
+            intro.hide();
+            containerPage.css({
+                height:Scense.computeWidth()
+            })
+        })
+
+        $window.on('resize',function(){
+            containerPage.css({
+                height:Scense.computeWidth()
+            })
+        })
+
     }
 }
